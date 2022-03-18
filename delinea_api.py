f0923783133843ad2aef4d8c4f3543a5a8771e16
lu-brian <101499851+lu-brian@users.noreply.github.com>
2022-03-18 02:15:10 -0700

    Update delinea_api.py

--- delinea_api.py
+++ delinea_api.py
@@ -2,80 +2,153 @@ from pprint import pprint
 from re import X
 import requests as r
 
-# Tenant Information [{tenant URL}, {tenantID}, {username}, {password}, {appID-optional}, {scope-optional}]
-# example:
-# t0 = ["abc1234.my.centrify.net","abc1234", "user@domain.com","password"]
-
+# tt0 = {
+#     'tenantURL':'', # required
+#     'tenantID': '', # required
+#     'username': '', # required
+#     'password': '', # optional: attempts password authentication
+#     'appID': '',    # optional: initiates oauth
+#     'appScope': ''  # optional: required if appID is set
+# }
 
 class Delinea_api():
 
-    def __init__(self, tenant: list, bearer: str | bool = False):
+    def __init__(self, tenant: dict, token_override: str | bool = False):
 
         self.tenant = tenant
 
         # Start Authentication
-        if bearer:
-            
-            self.bearer = bearer
+        # # bearer token override
+        if token_override:
+            self.bearer = token_override
+        
+        # # oauth authentication
+        elif (tenant["appID"] and tenant["appScope"]):
 
-        elif len(tenant) >= 5:
-
+            if not tenant['password']:
+                tenant['password'] = getpass('enter password: ')
+                
             head = {
-            "X-CENTRIFY-NATIVE-CLIENT": "true",
-            "Content-Type": "application/x-www-form-urlencoded",
+                "X-CENTRIFY-NATIVE-CLIENT": "true",
+                "Content-Type": "application/x-www-form-urlencoded",
             }
 
             key = {
                 "grant_type": "client_credentials",
-                "scope": tenant[5],
-                "client_secret": tenant[3],
-                "client_id": tenant[2]
+                "scope": tenant['appScope'],
+                "client_secret": tenant['password'],
+                "client_id": tenant['username']
                 }
 
-            self.bearer = (r.post(f"https://{tenant[0]}/Oauth2/Token/{tenant[4]}",headers=head,data=key).json())['access_token']
+            self.bearer = (r.post(f"https://{tenant['tenantURL']}/Oauth2/Token/{tenant['appID']}",headers=head,data=key).json())['access_token']
 
+        # # advanced authentication
         else:
-
             head = {
                 "X-CENTRIFY-NATIVE-CLIENT": "true",
                 "Content-Type": "application/json"
             }
 
             key = {
-                "TenantId": tenant[1],
-                "User": tenant[2],
+                "TenantId": tenant['tenantID'],
+                "User": tenant['username'],
                 "Version": "1.0"
                 }
 
-            response = r.post(f"https://{tenant[0]}/Security/StartAuthentication",headers=head,json=key).json()
-            sessionid: str = response["Result"]["SessionId"]
+            response = r.post(f"https://{tenant['tenantURL']}/Security/StartAuthentication",headers=head,json=key).json()
+            sessionid = response["Result"]["SessionId"]
             
-            mechanismid_00 = response["Result"]["Challenges"][0]['Mechanisms'][0]['MechanismId']
+            def processMechanisms(response):
+                mechanisms = response["Result"]["Challenges"][0]['Mechanisms']
+                userpass = list(filter(lambda mech: mech['Name'] == 'UP', mechanisms))
 
-            key = {
-                "TenantId": tenant[1],
-                "SessionId": sessionid,
-                "MechanismId": mechanismid_00,
-                "Action": "Answer",
-                "Answer": tenant[3]
-                }
+                # # automatic password authentication
+                if userpass and tenant["password"]:
 
-            adv_response = r.post(f"https://{tenant[0]}/Security/AdvanceAuthentication",headers=head,json=key)
-            print(adv_response)
+                    key = {
+                        "TenantId": tenant['tenantID'],
+                        "SessionId": sessionid,
+                        "MechanismId": userpass[0]['MechanismId'],
+                        "Action": "Answer",
+                        "Answer": tenant['password']
+                    }
 
-            self.bearer =  adv_response.cookies['.ASPXAUTH']
+                    response = r.post(f"https://{tenant['tenantURL']}/Security/AdvanceAuthentication",headers=head,json=key)
+                else:
+                    count = 0
+                    
+                    # # selection for mechanisms
+                    for mechanism in mechanisms:
+                        print(f'{count} | {mechanism["PromptMechChosen"]}')
+                        count += 1
+                    
+                    selection = input('# | > enter authentication (#): ')
 
-    # Post
-    def post(self, endpoint, key=None):
+                    try:
+                        while int(selection) >= count:
+                            print('# | ! authentication does not exist')
+                            selection = input('# | > enter authentication (#): ')
+                    except:
+                        print(f'# | ! not a number selection')
+                        exit()
+                    
+                    # # basic header template
+                    data = {
+                        "TenantId": tenant['tenantID'],
+                        "SessionId": sessionid,
+                        "MechanismId": mechanisms[int(selection)]['MechanismId'],
+                        "Action": 'Answer',
+                        "PersistentLogin": None
+                    }
+
+                    # # out of band
+                    if mechanisms[int(selection)]['AnswerType'] == 'StartTextOob':
+                        key = {
+                            **data,
+                            "Action": "StartTextOob",
+                        }
+                    # # secret question
+                    elif mechanisms[int(selection)]['Name'] == 'SQ':
+                        secrets = mechanisms[int(selection)]['MultipartMechanism']['MechanismParts']
+                        answer = { secret['Uuid']: getpass(f'# {selection} > {secret["QuestionText"]}\n# {selection} > ') for secret in secrets }
+
+                        key = {
+                            **data,
+                            "Answer": answer
+                        }
+                    # # password
+                    else:
+                        key = {
+                            **data,
+                            "Answer": getpass(f'# {selection} > enter password/pin: \n# {selection} > ')
+                        }
+
+                    response = r.post(f"https://{tenant['tenantURL']}/Security/AdvanceAuthentication",headers=head,json=key)
+
+                # # continue if bearer found, otherwise repeat with new mechanism
+                if '.ASPXAUTH' in response.cookies.keys():
+                    self.bearer = response.cookies['.ASPXAUTH']
+                    return None
+                else:
+                    return processMechanisms(response.json())
+            
+            processMechanisms(response)
+
+    # Request
+    def request(self, endpoint: str, key: dict | None = None):
+
+        header = {
+            "X-CENTRIFY-NATIVE-CLIENT": "true",
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Authorization": "Bearer " + self.bearer
+        }
         
-        if endpoint.lower() == "/redrock/query":
+        if endpoint.lower() == "/redrock/query" or endpoint.lower() == "redrock/query":
 
             headers = {
-                "X-CENTRIFY-NATIVE-CLIENT": "true",
-                "Content-Type": "application/json",
-                'Accept': "*/*",
-                "Accept-Encoding": "gzip, deflate, br",
-                'Authorization': "Bearer " + self.bearer
+                **header,
+                "Accept-Encoding": "gzip, deflate, br"
                 }
             
             query = {
@@ -89,22 +162,20 @@ class Delinea_api():
                     }
                 }
 
-            return r.post(f"https://{self.tenant[0]}/Redrock/query", headers=headers, json=query).json()
+            return r.post(f"https://{self.tenant['tenantURL']}/Redrock/query", headers=headers, json=query).json()
 
-        elif endpoint.lower() == "/core/makefile":
+        elif endpoint.lower() == "/core/makefile" or endpoint.lower() == "core/makefile":
 
             headers = {
-            "X-CENTRIFY-NATIVE-CLIENT": "true",
+            **header,
             "Content-Type": "application/x-www-form-urlencoded",
-            'accept': "*/*",
-            'Upgrade-Insecure-Requests': '1',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Authorization': "Bearer " + self.bearer
+            "Upgrade-Insecure-Requests": "1",
+            "Accept-Encoding": "gzip, deflate, br"
             }
 
-            rep = r.post(f"https://{self.tenant[0]}{endpoint}", headers=headers, data=key)
+            rep = r.post(f"https://{self.tenant['tenantURL']}/{endpoint}", headers=headers, data=key)
             
-            # test if response is good
+            # # test if response is good
             if rep.status_code == 200:
                 loc = input('\nFile Location (e.g. c:\\filename.txt): \n> ')
 
@@ -122,11 +193,4 @@ class Delinea_api():
 
         else:
 
-            headers = {
-            "X-CENTRIFY-NATIVE-CLIENT": "true",
-            "Content-Type": "application/json",
-            'accept': "*/*",
-            'Authorization': "Bearer " + self.bearer
-            }
-
-            return r.post("https://"+self.tenant[0]+endpoint, headers=headers, json=key).json()
+            return r.post(f"https://{self.tenant['tenantURL']}/{endpoint}", headers=header, json=key).json()
